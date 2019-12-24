44020ddadddfd13b1e924b6d2cb5a97a49bc9dec
imerla1 <giorgi1048@yahoo.com>
2019-12-25 00:03:36 +0400

    Second Commit

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user
 from app.auth import bp
-from app.auth.forms import RegistrationForm
+from app.auth.forms import RegistrationForm, LoginForm
 
 
 methods = ["GET", "POST"]
@@ -9,11 +9,22 @@ methods = ["GET", "POST"]
 @bp.route('/register', methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    err = form.password2.errors
-    print(err)
+   
    
     if form.validate_on_submit():
-        flash("Register Finished Succesfully")
-        return "HELLO WORLD"
+        flash("Registration finished succesfully", 'success')
+        return redirect(url_for('main.index'))
     return render_template('register.html', form=form)
     
+@bp.route('/login', methods=methods)
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        if username == "giorgi" and password == "1":
+            flash("You are Now logged in", 'success')
+            return redirect(url_for('main.index'))
+        else:
+            flash("Check username or password", 'danger')    
+    return render_template('login.html', form=form)
--- app/main/routes.py
+++ app/main/routes.py
@@ -4,4 +4,4 @@ from flask import render_template
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html')
+    return render_template('base.html', title="Lashas ")
