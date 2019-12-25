6213fbc024bb6f37b4691264323bf374fc513ad7
imerla1 <giorgi1048@yahoo.com>
2019-12-25 18:52:57 +0400

    add error Handlers and (404,403,500) html

--- app/__init__.py
+++ app/__init__.py
@@ -25,4 +25,7 @@ from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp)
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
+from app.errors import bp as error
+app.register_blueprint(error)
+
 from app import models
---
+++ app/errors/handlers.py
@@ -0,0 +1,15 @@
+#### Error Handlers
+from flask import render_template
+from app.errors import bp as error
+
+@error.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+@error.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+@error.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/404.html'), 500
